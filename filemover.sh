cb03145e993b6d6c2adb9ca9cc3881d86bdfc59d
Denis Arefyev <163993361+DenisArefyev@users.noreply.github.com>
2024-04-30 10:28:24 +0200

    Added issue#5 in filemover.sh

--- filemover.sh
+++ filemover.sh
@@ -11,3 +11,23 @@
 # Проверка, есть ли файлы с указанным расширением в исходной директории
 
 # Копирование файлов с указанным расширением в целевую директорию
+
+#!/bin/bash
+
+echo "Введите расширение файлов, которые нужно скопировать (например: txt, rtf, sh):"
+read file_extension
+
+echo "Введите путь к директории, куда скопировать файлы:"
+read target_directory
+
+files=$(find . -type f -name "*.$file_extension")
+
+if [ -z "$files" ]; then
+    echo "Файлов с расширением .$file_extension не найдено."
+else
+    echo "Найденные файлы с расширением .$file_extension:"
+    echo "$files"
+
+    echo "Копирование файлов в $target_directory..."
+    cp $files "$target_directory" && echo "Копирование завершено."
+fi
