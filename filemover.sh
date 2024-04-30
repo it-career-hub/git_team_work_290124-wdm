4fc50b15dc819e66be1a96204de23618bd49c76b
Denis Arefyev <163993361+DenisArefyev@users.noreply.github.com>
2024-04-30 11:38:16 +0200

    Update filemover.sh
    Группа #5 - убрали все лишнее.
    $files - скопирует все найденные файлы в указанную директорию

--- filemover.sh
+++ filemover.sh
@@ -12,20 +12,6 @@
 
 # Копирование файлов с указанным расширением в целевую директорию
 
-echo "Введите расширение файлов, которые нужно скопировать (например: txt, rtf, sh):"
-read file_extension
-
-echo "Введите путь к директории, куда скопировать файлы:"
-read target_directory
-
-files=$(find . -type f -name "*.$file_extension")
-
-if [ -z "$files" ]; then
-    echo "Файлов с расширением .$file_extension не найдено."
-else
-    echo "Найденные файлы с расширением .$file_extension:"
-    echo "$files"
-
-    echo "Копирование файлов в $target_directory..."
+echo "Копирование файлов в $target_directory..."
     cp $files "$target_directory" && echo "Копирование завершено."
 fi
